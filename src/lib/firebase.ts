497753ae13197f180ed2de0dec6aa37f3db48ebd
Nadeem <160517299+nadeemmhdm@users.noreply.github.com>
2026-02-07 17:44:38 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,14 @@ import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyBODXiKdzzbQtHFUvdcUkimCreUrvbu1Vs",
-    authDomain: "teste-77bc3.firebaseapp.com",
-    databaseURL: "https://teste-77bc3-default-rtdb.firebaseio.com",
-    projectId: "teste-77bc3",
-    storageBucket: "teste-77bc3.firebasestorage.app",
-    messagingSenderId: "272417985635",
-    appId: "1:272417985635:web:fe962e96c93c9f0d339456",
-    measurementId: "G-G4FHY4DT4Z"
+    apiKey: "#",
+    authDomain: "#",
+    databaseURL: "#",
+    projectId: "#",
+    storageBucket: "#",
+    messagingSenderId: "#",
+    appId: "#",
+    measurementId: "#"
 };
 
 const app = initializeApp(firebaseConfig);
